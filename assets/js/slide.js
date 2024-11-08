ee367d1e39c8c7ccf01298283efd3faf38d136d2
Jessica Ferraz <ferrazjaa@gmail.com>
2024-11-08 01:53:52 -0300

    update #5
    CSS:
    seções versões desktop depoimentos e simplify (tambem versão mobile)

---
+++ assets/js/slide.js
@@ -0,0 +1,29 @@
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-item');
+    if (index >= slides.length) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = slides.length - 1;
+    } else {
+        currentSlide = index;
+    }
+    const offset = -currentSlide * 52;
+    document.querySelector('.carousel-inner').style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+// Inicia o carrossel automático
+setInterval(() => {
+    nextSlide();
+}, 7000); // Troca a cada 3000ms (3 segundos)
+
+showSlide(currentSlide);
