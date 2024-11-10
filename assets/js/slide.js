cb1b7f5b5d2cfb6dbddda9b47ad73f7200c450b2
Jessica Ferraz <ferrazjaa@gmail.com>
2024-11-10 02:12:35 -0300

    update #7
    slider finalizado para telas menores

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -1,43 +1,16 @@
-let currentSlide = 0;
+let count = 1;
+document.getElementById("radio1").checked = true;
 
-function showSlide(index) {
-    const slides = document.querySelectorAll('.depoimentos__card');
-    const indicators = document.querySelectorAll('.dot span');
+setInterval (function(){
+    nextCard()
+},7000)
 
-    if (index >= slides.length) {
-        currentSlide = 0;
-    } else if (index < 0) {
-        currentSlide = slides.length - 1;
-    } else {
-        currentSlide = index;
+function nextCard() {
+    count++;
+    if(count>4){
+        count = 1;
     }
 
-    const offset = -currentSlide * 52; // Ajuste o valor de acordo com o tamanho do slide
-    document.querySelector('.carousel-inner').style.transform = `translateX(${offset}%)`;
+    document.getElementById("radio"+count).checked = true;
 
-    // Atualiza o indicador ativo
-    indicators.forEach(indicator => indicator.classList.remove('active'));
-    indicators[currentSlide].classList.add('active');
 }
-
-function nextSlide() {
-    showSlide(currentSlide + 1);
-}
-
-function prevSlide() {
-    showSlide(currentSlide - 1);
-}
-
-// Inicia o carrossel automático
-setInterval(() => {
-    nextSlide();
-}, 7000); // Troca a cada 7 segundos
-
-showSlide(currentSlide);
-
-// Adiciona funcionalidade de clique aos indicadores
-document.querySelectorAll('.carousel-indicators span').forEach((indicator, index) => {
-    indicator.addEventListener('click', () => {
-        showSlide(index);
-    });
-});
